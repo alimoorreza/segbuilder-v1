2405b4f63592fa456ee2a26921dc967acb2800f7
Eric Manley <ericm@thirdcoastcommodities.com>
2024-06-18 12:35:35 -0500

    callback code documentation

--- app/callbacks/ui_callbacks.py
+++ app/callbacks/ui_callbacks.py
@@ -313,18 +313,47 @@ def register_ui_callbacks(app):
             prevent_initial_call=True
     )
     def front_button_handle(n_clicks,selected_project,selected_image_name,curr_labels,current_new_masks_display):
+        """
+        Callback to handle the action of moving a mask to the front in the mask display.
+
+        Outputs:
+        - delete-button (n_clicks): Resets the click state of delete buttons (a list).
+        - mask-card-move-to-front (data): Data for the mask card to move to the front.
+        - mask-move-to-front (data): Data for the mask to move to the front.
+
+        Inputs:
+        - front-button (n_clicks): A list of the number of clicks on the 'Front' buttons for masks.
+
+        States:
+        - selected-project (selected_project): The name of the currently selected project.
+        - selected-image (selected_image_name): The name of the currently selected image.
+        - label-dropdown (curr_labels): List of current labels for the masks selected on their dropdowns.
+        - new-mask-display (current_new_masks_display): Section of the UI to display of new masks that have not yet been saved.
+        """
         logging.debug("front_button_handle triggered with %s",callback_context.triggered_id)
+        
+        # Check if any of the 'Front' buttons were actually clicked
         if not all(n is None for n in n_clicks):
             #logging.debug("something wasn't None")
             username = get_user_from_session()
             mask_num = callback_context.triggered_id["index"]
             image_obj = SB_project_image(username,selected_project,selected_image_name)
+
+            # Load the image and the mask to move
             image = image_obj.load_image()
             mask_to_move = image_obj.load_masks()[mask_num]
+
+            # Get label options for the project
             label_options = get_label_options(username,selected_project)
+
+            # Create a new mask card for the mask to move
             new_card = create_mask_cards(image,[mask_to_move],[curr_labels[mask_num]],label_options=label_options,new_masks=True,index_offset=len(current_new_masks_display))
+
+            # Reset the click state of the delete buttons
             delete_clicks = [None]*len(n_clicks)
             delete_clicks[mask_num] = 1
+
+            # Return the updated delete clicks, new mask card, and mask to move
             return delete_clicks, new_card, mask_to_move
         return [no_update]*len(n_clicks), no_update, no_update
 
@@ -336,15 +365,37 @@ def register_ui_callbacks(app):
             prevent_initial_call=True
     )
     def edit_button_handle(n_clicks,selected_project,selected_image_name):
+        """
+        Callback to handle the action of editing a mask in the selected project and image.
+        This takes a mask, converts it to a polygon that is then displayed on the mask-editing UI.
+        This allows it to be saved just like a manually-drawn mask.
+
+        Outputs:
+        - edit-button-polygon-data (data): The polygon data for the mask we're editing
+
+        Inputs:
+        - edit-button (n_clicks): Number of clicks on the 'Edit' buttons for masks (a list).
+
+        States:
+        - selected-project (selected_project): The name of the currently selected project.
+        - selected-image (selected_image_name): The name of the currently selected image.
+        """
         logging.debug("edit_button_handle triggered with %s",callback_context.triggered_id)
+
+        # Check if any of the 'Edit' buttons were actually clicked
         if not all(n is None for n in n_clicks):
-            #logging.debug("something wasn't None")
+            
             username = get_user_from_session()
             mask_num = callback_context.triggered_id["index"]
+
+            # Load the mask to be edited
             image_obj = SB_project_image(username,selected_project,selected_image_name)
-            #image = image_obj.load_image()
             mask_to_edit = image_obj.load_masks()[mask_num]
+
+            # Convert the mask segmentation data into contours
             contours = contours_from_mask(mask_to_edit["segmentation"])
+
+            # Convert contours into polygon shapes suitable for Plotly
             shapes = plotly_shapes_from_contours(contours)
 
             logging.debug("SHAPES for editing: %s",shapes)
@@ -358,8 +409,6 @@ def register_ui_callbacks(app):
         Output('new-mask-store','data'),
         Output("mask-composite-image","src"),
         Output("mask-image","src"),
-        #Output({'type': 'new-delete-button', 'index': ALL}, 'n_clicks'),
-
         
         Input('selected-image','data'),
         Input('generate-manual-mask-button','n_clicks'),
@@ -368,8 +417,6 @@ def register_ui_callbacks(app):
         Input({'type': 'new-front-button', 'index': ALL}, 'n_clicks'),
         Input("save-notify",'children'),
         Input("edit-button-polygon-data","data"),
-        
-
 
         State('selected-project','data'),
         State('new-mask-store','data'),
@@ -379,26 +426,56 @@ def register_ui_callbacks(app):
         State({'type': 'new-label-dropdown', 'index': ALL}, 'value'),
         State("mask-move-to-front","data"),
         State('graph-draw','figure'),
-        #State("session","data"),
         prevent_initial_call=True
     )
     def update_annotation_page(selected_image_name,generate_manual_n_clicks,gen_composite_n_clicks,new_front_mask_card,nfb_n_clicks,save_notify,edit_button_polygon_data,selected_project,current_new_masks,current_new_display_cards,closed_paths,labels,new_labels,new_front_mask,curr_fig_image):
+        """
+        Callback to update the annotation page based on various user actions such as selecting an image, generating masks, moving masks, and saving changes.
+
+        Outputs:
+        - mask-display (children): The part of the UI where the image masks are displayed.
+        - graph-draw (figure): The part of the UI where the user can draw new masks on the image - it is a Plotly figure.
+        - new-mask-display (children): The part of the UI where newly generated masks (such as those draw by the user) are displayed - these will be lost if the save button is not clicked.
+        - new-mask-store (data): Browser data store for storing newly-generated masks.
+        - mask-composite-image (src): The image which is a combination of any labeled masks, drawn partially transparent over the top of the original image.
+        - mask-image (src): The combination of labeled masks displayed as an image with the assigned colors - same as above but without the transparency and original image underneath.
+
+        Inputs:
+        - selected-image (selected_image_name): The name of the selected image.
+        - generate-manual-mask-button (generate_manual_n_clicks): Number of clicks on the button to generate manual mask.
+        - generate-composite-image-button (gen_composite_n_clicks): Number of clicks on the button to generate composite images.
+        - mask-card-move-to-front (new_front_mask_card): Browser data for a mask card that is moving to the front.
+        - new-front-button (nfb_n_clicks): Number of clicks on the front buttons for any newly generated masks (a list).
+        - save-notify (save_notify): Whether the notification for a recent save was completed - so everything can be reloaded from the save to ensure consistency.
+        - edit-button-polygon-data (edit_button_polygon_data): Browser data store for polygon shapes for masks being edited.
+
+        States:
+        - selected-project (selected_project): The name of the currently selected project.
+        - new-mask-store (current_new_masks): Browser data for any newly generated masks.
+        - new-mask-display (current_new_display_cards): The part of the UI where newly generated masks (such as those draw by the user) are displayed
+        - closed-paths-store (closed_paths): Browser data for closed paths (polygons) used in manual mask creation.
+        - label-dropdown (labels): Current labels for the masks that were loaded from memory (a list).
+        - new-label-dropdown (new_labels): Current labels for any newly generated masks (a list).
+        - mask-move-to-front (new_front_mask): Browser data store for the mask to move to the front.
+        - graph-draw (curr_fig_image): The Plotly figure where the user may have drawn or edited masks.
+        """
+        #!!TODO: This is a massive callback because there are so many things that trigger changes to the annotations tab. We should consider decomposing some of the code into utility functions to keep it better organized.
         username = get_user_from_session()
         no_update_ALL = [no_update]*len(nfb_n_clicks)
         logging.debug("update_annotation_page selected_image_name %s",selected_image_name)
 
+        # Handle editing an existing mask
         if callback_context.triggered_id == "edit-button-polygon-data":
             if isinstance(curr_fig_image, dict): #check if the image ends up getting saved as a dict in dcc.Store
                 curr_fig_image = go.Figure(curr_fig_image)
             curr_fig_image.update_layout(shapes=edit_button_polygon_data,dragmode="drawclosedpath")
-            #print(edit_button_polygon_data)
-            #curr_fig_image.show()
             return no_update, curr_fig_image, no_update, no_update, no_update, no_update
-        #load everything if they selected a new image or saved
+        
+        # Handle selecting a new image or completing a save - load everything 
         elif callback_context.triggered_id == "selected-image" or callback_context.triggered_id == "save-notify":
             if not selected_image_name:
                 raise PreventUpdate
-                #return None, None, None, None, None, None #, no_update_ALL
+
             else:
                 pen_color = "white"
                 image_obj = SB_project_image(username,selected_project,selected_image_name)
@@ -416,7 +493,7 @@ def register_ui_callbacks(app):
                                             #margin=dict(t=0, b=5, l=5, r=5),  # Set the top margin to 0
                                             #title=dict(text='', pad=dict(t=0, b=0, l=0, r=0)),
                                             newshape=dict(line=dict(color=pen_color)))
-                    #logging.debug("fig_image",fig_image)
+
                     segmented_image = ""
                     composite_image = ""
                     if image_obj.has_segmented_image():
@@ -441,6 +518,8 @@ def register_ui_callbacks(app):
 
                     return [], fig_image, [], None, composite_image, segmented_image #, no_update_ALL
         
+        # Handle when the user clicks the "Generate Manual Mask" button after having
+        # drawn on the image with the Plotly closed path tool
         elif callback_context.triggered_id == "generate-manual-mask-button":
             image_obj = SB_project_image(username,selected_project,selected_image_name)
             image = image_obj.load_image()
@@ -459,6 +538,7 @@ def register_ui_callbacks(app):
             return no_update, no_update, new_display_cards, new_masks_store, no_update, no_update #, no_update_ALL
         
 
+        # Handle a click on the "Generate Composite Mask Image" button
         elif callback_context.triggered_id == "generate-composite-image-button":
             image_obj = SB_project_image(username,selected_project,selected_image_name)
             image = image_obj.load_image()
@@ -467,11 +547,12 @@ def register_ui_callbacks(app):
             mask_image = np.zeros_like(image)
             label_colors_dict = get_label_colors_dict(username,selected_project)
             
-
+            # loop through the masks from back to front
             for i in range(len(masks)-1,-1,-1):
-                #logging.debug("i",i)
+                
                 curr_label = labels[i]
-                #logging.debug("curr_label",curr_label)
+                
+                # skip over deleted masks
                 if curr_label != "DELETE":
                     curr_mask = np.array(masks[i]["segmentation"])
                     
@@ -480,6 +561,7 @@ def register_ui_callbacks(app):
                     colored_mask[curr_mask > 0] = color
                     mask_image = np.where(curr_mask[..., np.newaxis] > 0, colored_mask, mask_image)
 
+            # check if there are any masks in the "new mask" area and loop through them backwards
             if current_new_masks:
                 for i in range(len(current_new_masks)-1,-1,-1):
                     curr_label = new_labels[i]
@@ -490,11 +572,13 @@ def register_ui_callbacks(app):
                         colored_mask[curr_mask > 0] = color
                         mask_image = np.where(curr_mask[..., np.newaxis] > 0, colored_mask, mask_image)
 
+            # generate the composite image from the combined mask image and the original image
             composite_image, segmented_image = make_composite_image(image,mask_image)
 
 
             return no_update, no_update, no_update, no_update, composite_image, segmented_image #, no_update_ALL
         
+        # Handle when the user clicks to move an existing mask to the front
         elif callback_context.triggered_id == "mask-card-move-to-front":
             new_display_cards = new_front_mask_card
             if current_new_display_cards:
@@ -504,7 +588,7 @@ def register_ui_callbacks(app):
                 new_masks_store += current_new_masks
             return no_update, no_update, new_display_cards, new_masks_store, no_update, no_update #, no_update_ALL
         
-
+        # Handle when the user clicks to move a mask in the "new mask" area to the front
         elif 'type' in callback_context.triggered_id and callback_context.triggered_id["type"] == "new-front-button" and not(all(n is None for n in nfb_n_clicks)):
             image_obj = SB_project_image(username,selected_project,selected_image_name)
             image = image_obj.load_image()
@@ -526,32 +610,38 @@ def register_ui_callbacks(app):
         return no_update, no_update, no_update, no_update, no_update, no_update #, no_update_ALL
 
     @app.callback(
-        #Output('canvas-message', 'children'),
         Output('drawings-store','data'),
         Output('closed-paths-store','data'),
         Input('graph-draw', 'relayoutData'),
-        #State('image-store','data'),
-        #State('canvas','height'),
-        #State('canvas','width'),
         prevent_initial_call=True
     )
     def update_click_coordinates(shape_data):
-        #print("SHAPE DATA",shape_data)
-        #image = np.array(image)
-        #image = image.astype(np.uint8)
-        #logging.debug(type(shape_data))
-        logging.debug("shape data  %s",shape_data)
-        #image_width = image.shape[0]
-        #image_height = image.shape[1]
+        """
+        Callback to update the coordinates of manually drawn/edited masks and store the closed paths so
+        they can be used by later callbacks
 
-        #image_scale_factor = image_width/CANVAS_WIDTH
-        
-        #logging.debug("image:",image.shape)
-        #logging.debug("canvas:",canvas_height,", ",canvas_width)
-        coords = []
-        closed_paths = []
+        Outputs:
+        - drawings-store (data): List of coordinates for the drawn shapes - TODO: We're not currently using this, but it's here so we can later use non-closed paths for something else (e.g., generating a new SAM mask)
+        - closed-paths-store (data): List of closed paths' coordinates - this is what we're using in later callbacks
+
+        Inputs:
+        - graph-draw (shape_data): Data from the relayout event of the Plotly figure where the user may have drawn or edited masks.
+
+        Notes:
+        - This callback processes different types of shapes (circles and paths) and updates the coordinates accordingly.
+        - Closed paths are identified and stored separately.
+        """
+        logging.debug("shape data  %s",shape_data)
+
+
+        coords = [] # for non-closed paths
+        closed_paths = [] # for closed paths
+
+        # check if we actually have any drawn shapes
         if "shapes" in shape_data:
             for shape in shape_data["shapes"]:
+
+                # in case we use the circle drawing tool later
                 if shape["type"] == "circle":
                     include = 1
                     if shape["line"]["color"] == "red":
@@ -559,20 +649,26 @@ def register_ui_callbacks(app):
                     x = (shape["x0"]+shape["x1"])/2
                     y = (shape["y0"]+shape["y1"])/2
                     coords.append([include,x,y])
+
+                # check if the user has drawn a path
                 if shape["type"] == "path":
-                    #closed path
+                    #check if it is a closed path (format specifies that it ends in "Z")
                     if shape["path"][-1] == "Z":
                         indices_str = [el.replace("M", "").replace("Z", "").split(",") for el in shape["path"].split("L")]
                         closed_path_indices = [(float(p[0]),float(p[1])) for p in indices_str]
                         closed_paths.append(closed_path_indices)
+
+                    # in case we later support a non-closed path
                     else:
                         include = 1
                         if shape["line"]["color"] == "red":
                             include = 0 
                         indices_str = [el.replace("M", "").replace("Z", "").split(",") for el in shape["path"].split("L")]
                         indices_float = [[include,float(p[0]),float(p[1])] for p in indices_str]
-                        #logging.debug(indices_float)
+                        
                         coords += indices_float
+                        
+        # another place closed paths could be
         for k in shape_data:
             if 'shapes[' in k and '].path' in k and shape_data[k][-1] == "Z":
                 indices_str = [el.replace("M", "").replace("Z", "").split(",") for el in shape_data[k].split("L")]
