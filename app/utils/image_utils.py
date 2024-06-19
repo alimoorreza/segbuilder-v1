c355f441907e6a8ad581620c8c528d59df43764f
Eric Manley <ericm@thirdcoastcommodities.com>
2024-06-18 22:13:41 -0500

    utility function documentation

--- app/utils/image_utils.py
+++ app/utils/image_utils.py
@@ -5,43 +5,82 @@ from matplotlib.path import Path
 import plotly.express as px
 
 def create_mask_from_paths(path_coordinates, img_shape):
-    #print("path_coordinates",path_coordinates)
-    #print("img_shape",img_shape)
-    height, width, _ = img_shape
-    #y, x = np.mgrid[:height, :width]
-    #points = np.vstack((x.ravel(), y.ravel())).T
-    x, y = np.meshgrid(np.arange(width),np.arange(height))
+    """
+    Create a binary mask from path coordinates.
 
+    This function generates a binary mask of the specified image shape, with True values
+    inside the paths defined by the path coordinates.
+
+    :param path_coordinates: A list of path coordinates, where each path is defined by a list of (x, y) tuples.
+    :param img_shape: The shape of the image (height, width, channels).
+    :return: A binary mask with True values inside the paths.
+    """
+    height, width, _ = img_shape
+
+    # Create a meshgrid of (x, y) coordinates
+    x, y = np.meshgrid(np.arange(width),np.arange(height))
     x, y = x.flatten(), y.flatten()
 
+    # Stack the flattened x and y coordinates into a single array of points
+    # Each point is represented as a (x, y) tuple
     points = np.vstack((x,y)).T
 
-
+    # Initialize an empty binary mask of the same height and width as the image
     mask = np.zeros((height, width), dtype=bool)
+
+    # Iterate over each set of path coordinates
     for path_coords in path_coordinates:
+        # Create a Path object from the path coordinates
         path = Path(path_coords)
+        # Use the Path object to determine which points are inside the path
+        # Update the mask with True values where points are inside the path
         mask |= path.contains_points(points).reshape(height, width)
-
-
 
     return mask
 
 def encode_img_for_display(cv2rgbimg):
+    """
+    Encode an image for display in the application.
+
+    This function encodes a CV2 RGB image as a PNG and then base64 encodes the PNG for display.
+
+    :param cv2rgbimg: The CV2 RGB image to encode.
+    :return: The base64 encoded PNG image as a string.
+    """
     _, buffer = cv2.imencode('.png', cv2rgbimg)
-    #
     encoded_image = base64.b64encode(buffer).decode('utf-8')
     return encoded_image
 
 
 def contours_from_mask(mask):
+    """
+    Extract contours from a binary mask.
+
+    This function finds and returns the contours in a binary mask using OpenCV.
+
+    :param mask: The binary mask from which to extract contours.
+    :return: A list of contours found in the mask.
+    """
     contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
 def plotly_shapes_from_contours(contours,pen_color="purple"):
+    """
+    Convert contours into Plotly shape dictionaries.
+
+    This function takes a list of contours and converts each contour into a Plotly shape dictionary
+    with a specified pen color. The shapes can be used to overlay contours on Plotly figures.
+
+    :param contours: A list of contours, where each contour is an array of (x, y) coordinates.
+    :param pen_color: The color of the contour lines (default is "purple").
+    :return: A list of Plotly shape dictionaries representing the contours.
+    """
     shapes = []
     for contour in contours:
-        # Each contour is a shape
+        # Construct the path string for the contour
         path = 'M ' + ' L '.join(f"{x[0][0]},{x[0][1]}" for x in contour) + ' Z'
+
+        # Create a dictionary representing the shape
         shape = dict(
             type="path",
             path=path,
@@ -49,22 +88,44 @@ def plotly_shapes_from_contours(contours,pen_color="purple"):
             fillcolor="rgba(255, 0, 0, 0.4)",  # Semi-transparent fill
             editable = True
         )
+
+        # Append the shape dictionary to the list
         shapes.append(shape)
     return shapes
 
 
 def create_checkerboard(image_shape, square_size=50):
+    """
+    Create a checkerboard pattern image to indicate transparent background areas of masks.
+
+    This function generates a checkerboard pattern image of the specified shape and square size.
+
+    :param image_shape: The shape of the image (rows, cols, channels).
+    :param square_size: The size of each square in the checkerboard pattern (default is 50).
+    :return: A checkerboard pattern image as a NumPy array.
+    """
     rows, cols, _ = image_shape
     checkerboard = np.zeros((rows, cols), dtype=np.uint8)
     for i in range(rows):
         for j in range(cols):
             if (i // square_size) % 2 == (j // square_size) % 2:
-                checkerboard[i, j] = 200
+                checkerboard[i, j] = 150
             else:
-                checkerboard[i, j] = 100
+                checkerboard[i, j] = 50
     return checkerboard
 
 def apply_mask_to_image(image, mask, square_size=50):
+    """
+    Apply a mask to an image and overlay a checkerboard pattern where the mask is False.
+
+    This function overlays a checkerboard pattern on the regions of the image where the mask is False.
+    It ensures that the mask is a boolean array and creates a 3-channel checkerboard pattern.
+
+    :param image: The original image as a NumPy array.
+    :param mask: The binary mask as a NumPy array.
+    :param square_size: The size of each square in the checkerboard pattern (default is 50).
+    :return: The image with the checkerboard pattern applied where the mask is False.
+    """
     # Ensure the mask is a boolean array
     mask = mask.astype(bool)
     
@@ -81,11 +142,31 @@ def apply_mask_to_image(image, mask, square_size=50):
 
 
 def get_cv2_image(filename):
+    """
+    Read an image using OpenCV.
+
+    This function reads an image from the specified file using OpenCV. The image is returned
+    in its original color space (BGR).
+
+    :param filename: The path to the image file.
+    :return: The image as a NumPy array.
+    """
     img = cv2.imread(filename)
+    # The following line is needed if we have to convert the image to RGB - I don't think we need to, though
     #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
 def add_meta_info_to_masks(flat_masks):
+    """
+    Add metadata information to a list of masks.
+
+    This function takes a list of masks and adds metadata information to each mask.
+    Currently, it adds the segmentation data to each mask's metadata dictionary.
+    Additional metadata could be added as needed.
+
+    :param flat_masks: A list of masks (binary masks as NumPy arrays).
+    :return: A list of metadata-enhanced masks (dictionaries containing segmentation data).
+    """
     meta_masks = []
     for mask_num in range(len(flat_masks)):
         curr_meta_mask = {}
@@ -95,18 +176,49 @@ def add_meta_info_to_masks(flat_masks):
     return meta_masks
 
 def hex_to_rgb(hex_color):
+    """
+    Convert a hex color string to an RGB list.
+
+    This function converts a hex color string (e.g., "#RRGGBB") to a list of RGB values.
+    The hex color string is stripped of its leading '#' character, and each pair of hex digits
+    is converted to an integer representing the red, green, and blue color components.
+
+    :param hex_color: The hex color string to convert (e.g., "#RRGGBB").
+    :return: A list of RGB values [R, G, B].
+    """
     hex_color = hex_color.lstrip('#')
     return [int(hex_color[i:i+2], 16) for i in (0, 2 ,4)]
 
 
 def make_composite_image(image,mask_image):
+    """
+    Create a composite image by blending an original image with a mask image.
+
+    This function converts the original image to grayscale and then blends it with the mask image.
+    The masked areas are highlighted while the rest of the image remains in grayscale.
+    The resulting composite and mask images are encoded as base64 strings for display in a web application.
+
+    :param image: The original image as a NumPy array (BGR color space).
+    :param mask_image: The mask image as a NumPy array.
+    :return: A tuple containing the composite image and the display mask image, both encoded as base64 strings.
+    """
+    # Convert the original image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray_image = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
+
+    # Create a background image that is grayscale where the mask is not present
     gray_background_image = np.where(mask_image > 0, mask_image, gray_image)
+
+    # Initialize the composite image with the same shape as the original image
     mask_composite_image = np.zeros_like(image)
+
+    # Blend the grayscale background image with the mask image to create the composite image
     mask_composite_image = cv2.addWeighted(gray_background_image,0.95,mask_image,0.05,0)
+
+    # Encode the composite image and segmented mask image for display
     mask_composite_image = 'data:image/png;base64,{}'.format(encode_img_for_display(cv2.cvtColor(mask_composite_image, cv2.COLOR_BGR2RGB)))
     display_mask_image = 'data:image/png;base64,{}'.format(encode_img_for_display(cv2.cvtColor(mask_image, cv2.COLOR_BGR2RGB)))
+    
     return mask_composite_image, display_mask_image
 
 
--- app/utils/ui_utils.py
+++ app/utils/ui_utils.py
@@ -13,22 +13,49 @@ IMG_WIDTH = 300
 #IMG_HEIGHT = 400
 
 def generate_label_cards(username,project_name):
+    """
+    Generate label cards for a given project and user.
+
+    This function retrieves label records from the database and generates Dash Bootstrap Components (dbc) cards
+    for each label. The cards are styled with the label's color and name.
+
+    :param username: The username of the user.
+    :param project_name: The name of the project.
+    :return: A list of dbc.Card objects representing the labels.
+    """
+    # Retrieve label records from the database
     db_label_records = get_db_item(table_name="project-classes",key_name="username-projectname",key_value=(username+"-"+project_name),default_return={"classes":[]})
     label_records = db_label_records["classes"]
-    #!! delete after testing
-    #label_records = classes_table.get_item(Key={'username-projectname':username+"-"+project_name})["Item"]["classes"]
+
+    # Iterate over each label record and create a dbc.Card for each label and append into a running list
     label_cards = []
     for r in label_records:
-        #logging.debug("record label",r)
         label_card = dbc.Card([
-            dbc.CardBody(style={"backgroundColor":"rgb({},{},{})".format(*r["color"])}),
-            dbc.CardFooter(r["name"],style={"textAlign":"center"})
+            dbc.CardBody(style={"backgroundColor":"rgb({},{},{})".format(*r["color"])}), # Set background color to label color
+            dbc.CardFooter(r["name"],style={"textAlign":"center"}) # Set label name as card footer
         ],style={"width":"8rem","height":"8rem","float":"left","marginLeft":"1rem"})
         label_cards.append(label_card)
     return label_cards
 
 def create_mask_cards(img, masks, labels, label_options = ["unlabeled"], new_masks=False, index_offset = 0):
+    """
+    Create mask cards for displaying image masks with label options.
+
+    This function generates Dash Bootstrap Components (dbc) cards for each mask, with dropdowns for selecting labels.
+    It also includes buttons for moving, deleting, and editing masks.
+
+    :param img: The original image as a NumPy array.
+    :param masks: A list of masks as NumPy arrays.
+    :param labels: A list of labels corresponding to each mask.
+    :param label_options: A list of label options for the dropdowns (default is ["unlabeled"]).
+    :param new_masks: A flag indicating whether the masks are new (default is False).
+    :param index_offset: An offset for the mask indices (default is 0).
+    :return: A list of dbc.Card objects representing the masks with label options.
+    """
+    # Convert the image from BGR to RGB color space
     image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Define ID types for the different components based on whether the masks are new
     id_type = "label-dropdown"
     front_button_id_type = "front-button"
     delete_button_id_type = "delete-button"
@@ -39,6 +66,8 @@ def create_mask_cards(img, masks, labels, label_options = ["unlabeled"], new_mas
         front_button_id_type = "new-front-button"
         delete_button_id_type = "new-delete-button"
         card_id_type = "new-mask-card"
+
+    # a list to store the mask cards
     card_list = []
    
     #if there aren't enough labels for all the masks,
@@ -46,6 +75,10 @@ def create_mask_cards(img, masks, labels, label_options = ["unlabeled"], new_mas
     #only will work if there is at least one label
     if len(labels) < len(masks):
         labels = labels+([labels[-1]]*(len(masks)-len(labels)))
+    
+    # Iterate over each mask and create a "card" for each one that contains a
+    # dropdown with all of its class labels as well as front, edit, and delete buttons.
+    # We're calling it a card, but it isn't actually a dbc.Card - just a <div> tag with appropriate styling
     for idx in range(len(masks)):
         label_idx = idx+index_offset
         curr_card = html.Div([
@@ -67,38 +100,40 @@ def create_mask_cards(img, masks, labels, label_options = ["unlabeled"], new_mas
 
 
 def populate_project_cards(username):
+    """
+    Populate project cards for a given user.
+
+    This function retrieves the user's projects from the database and generates a list of Dash HTML 
+    Div elements, each containing a project card. Each card displays the project's cover image 
+    and project name.
+
+    :param username: The username of the user.
+    :return: A list of Dash HTML Div elements representing the project cards.
+    """
     logging.debug("SBDEBUG: inside populate_project_cards")
     project_cards = []
     curr_projects = []
 
-
-    #!! moved this code to database.py - delete after testing
-    #dynamodb = get_dynamodb_resource()
-    #projects_table = dynamodb.Table("projects")
-    #curr_projects_response = projects_table.get_item(Key={"username":username})
-    #logging.debug("In populate_project_cards: curr_projects_response %s",curr_projects_response)
-    #if "Item" in curr_projects_response:
-    #    curr_projects = curr_projects_response["Item"]["projects"]
-
+    # Retrieve the current projects for the user from the database
     db_results = get_db_item(table_name="projects",key_name="username",key_value=username,default_return={"projects":[]})
     curr_projects = db_results["projects"]
-    print("CURR PROJECTS:",curr_projects)
+    #print("CURR PROJECTS:",curr_projects)
 
-    
-    #for prefix in result.search('CommonPrefixes'):
+    # Iterate over each project
     for proj in curr_projects:
-        #logging.debug("prefix",prefix)
-        #foldername = prefix.get('Prefix')[len(username)+1:-1]
+        
         sb_project = SB_project(username,proj)
         logging.debug("SBDEBUG: about to create a project card")
         cover_image_url = sb_project.get_cover_image_url()
         logging.debug("SBDEBUG: sb_project.get_cover_image_url() - %s",cover_image_url)
 
-        curr_card = html.Div(id={'type':"project-card",'index':proj},children=dbc.Card([
-            
-            dbc.CardImg(src=cover_image_url,style={"height":"14rem"}),
-            dbc.CardBody(html.H4(proj,className="card-title"),style={"textAlign":"center"})
-        ],style={"height":"18rem","width": "18rem"}),style={"float":"left","paddingLeft":"20px"})
+        # Create a Dash Bootstrap Card for the project
+        curr_card = html.Div(id={'type':"project-card",'index':proj},
+            children=dbc.Card([
+                dbc.CardImg(src=cover_image_url,style={"height":"14rem"}),
+                dbc.CardBody(html.H4(proj,className="card-title"),style={"textAlign":"center"})
+            ],style={"height":"18rem","width": "18rem"}),style={"float":"left","paddingLeft":"20px"}
+        )
         logging.debug("SBDEBUG: finished createing project card")
         project_cards.append(curr_card)
 
@@ -110,41 +145,82 @@ def populate_project_cards(username):
 
 
 def populate_files(username,project_name):
+    """
+    Populate the file list for a given project and user.
+
+    This function retrieves the list of image files for the specified project and user.
+    It generates a list of Dash Bootstrap Components (dbc) ListGroupItems for each file, 
+    indicating whether the file has masks or segmented images.
+
+    :param username: The username of the user.
+    :param project_name: The name of the project.
+    :return: A list of dbc.ListGroupItem objects representing the files.
+    """
     image_list = []
+
+    # Create an SB_project object for the specified project
     project_object = SB_project(username,project_name)
+
+    # Get the list of image filenames for the project
     filelist = project_object.get_image_names()
     
     for file_idx in range(len(filelist)):
         file = SB_project_image(username,project_name,filelist[file_idx])
         item_color = None
+
+        # Check if the file has masks (it's "ready") and set item color to primary if true
         if file.has_masks():
             item_color = "primary"
+
+        # Check if the file has segmented images (it's "done") and set item color to success if true
         if file.has_segmented_image():
             item_color = "success"
+
         image_list.append(dbc.ListGroupItem(dbc.Row([
                 dbc.Col(dcc.Checklist(options=[""],value=[],id={'type':"file-item-checklist",'index':file.get_filename()}),width="auto"),
                 dbc.Col(html.Div(file.get_filename(),id={'type':"file-item",'index':file.get_filename()})),
             ]),color=item_color)
         )
     
-    
     return image_list
 
 
 
 def get_label_options(username,project):
+    """
+    Retrieve class label options for a given project.
 
+    This function fetches the label classes from the database for the specified project.
+    It extracts the names of the classes and returns them as a list of label options.
+
+    :param username: The username of the user.
+    :param project: The name of the project.
+    :return: A list of label names.
+    """
     classes_from_db = get_db_item(table_name="project-classes",key_name="username-projectname",key_value=(username+"-"+project),default_return={"classes":[]})
     classes_list = classes_from_db["classes"]
+
+    # they all have a name and a color, but we just need the names in this function
     label_options = [n['name'] for n in classes_list]
     return label_options
 
 
 def get_label_colors_dict(username,project):
+    """
+    Retrieve a dictionary of class label colors for a given project.
+
+    This function fetches the label classes from the database for the specified project and user.
+    It creates a dictionary mapping label names to their corresponding RGB color tuples.
+
+    :param username: The username of the user.
+    :param project: The name of the project.
+    :return: A dictionary mapping label names to RGB color tuples.
+    """
     label_colors_dict = {}
     classes_from_db = get_db_item(table_name="project-classes",key_name="username-projectname",key_value=(username+"-"+project),default_return={"classes":[]})
     classes_list = classes_from_db["classes"]
 
+    # Populate the dictionary with label names and their corresponding color tuples
     for entry in classes_list:
         label_colors_dict[entry["name"]] = tuple(entry["color"])
     return label_colors_dict
