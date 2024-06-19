13f91fc96a8f7b3fbc8a13ddd8b81321d9ccca02
Eric Manley <ericm@thirdcoastcommodities.com>
2024-06-18 19:13:24 -0500

    SB_project class comments

--- app/project_models/project_management.py
+++ app/project_models/project_management.py
@@ -208,7 +208,23 @@ class SB_project_image:
             write_file(self.__segments_path,img_bytes)
 
 class SB_project:
+    """
+    Class for managing projects for a given user, including images, image masks, and segmented images according to label-color schemes.
+
+    Attributes:
+    - __username (str): The username of the user.
+    - __project_name (str): The name of the project.
+    - __images_dir_path (str): The directory path for the project's images.
+    - __segmented_images_dir_path (str): The directory path for the project's segmented images.
+    """
+    
     def __init__(self, username, project_name):
+        """
+        Initialize a new SB_project instance.
+
+        :param username: The username of the user.
+        :param project_name: The name of the project.
+        """
         logging.debug("SBDEBUG: creating SB_project for"+str(project_name))
         self.__username = username
         self.__project_name = project_name
@@ -216,9 +232,22 @@ class SB_project:
         self.__segmented_images_dir_path = "segmented_images/"+self.__username+"/"+self.__project_name
 
     def get_image_names(self):
+        """
+        Get the names of the images in the project's image directory.
+
+        :return: A list of image filenames.
+        """
         return get_files_in_directory(self.__images_dir_path)
     
     def get_cover_image_url(self):
+        """
+        Get the URL for the project's cover image.
+
+        If the project has images, the first image is used as the cover image. 
+        Otherwise, a default logo image is used.
+
+        :return: The URL of the cover image.
+        """
         logging.debug("SBDEBUG: About to get the cover image for "+self.__project_name)
         cover_image = "assets/eyelogo.png"
         image_names = get_files_in_directory(self.__images_dir_path)
@@ -230,50 +259,79 @@ class SB_project:
         return cover_image
     
     def prepare_download(self,file_list,zipfilename):
+        """
+        Prepare the project files for download by creating a ZIP archive.
+
+        The method creates temporary directories for the project files, downloads the specified files,
+        and then archives them into a ZIP file.
+
+        :param file_list: A list of filenames to be included in the download.
+        :param zipfilename: The name of the ZIP file to create.
+        :return: The path to the created ZIP file.
+        """
+        # Remove the user's temporary directory if it exists
         if os.path.exists("tmp/"+self.__username):
             shutil.rmtree("tmp/"+self.__username)
+
+        # Create necessary directories for downloading and archiving files
         if not os.path.exists("tmp/downloads"):
             os.makedirs("tmp/downloads")
         os.makedirs("tmp/"+self.__username+'/'+self.__project_name+'/images')
         os.makedirs("tmp/"+self.__username+'/'+self.__project_name+'/segmented_images')
         os.makedirs("tmp/"+self.__username+'/'+self.__project_name+'/image_masks')
 
+        # Download each file in the file list
         for filename in file_list:
             #strip out leading space
             if filename[0] == " ":
                 filename = filename[1:]
             filename_plus_png = filename
+
+            # figure out the name that the segmented image will have - it's the base filename plus png (even if the original was a jpg)
             if len(filename) > 4 and filename[-4] == ".":
                 filename_plus_png = filename[:-4]
             filename_plus_png += ".png"
-            filename_plus_sgdbi = filename
+
+            # figure out the name of the SegBuilder archive file (.sgbdi) - it's the base file name plus .sgbdi
+            filename_plus_sgbdi = filename
             if len(filename) > 4 and filename[-4] == ".":
                 filename_plus_sgbdi = filename[:-4]
             filename_plus_sgbdi += ".sgbdi"  
+
+
+            # download the original image file
             try:
                 file_download(self.__images_dir_path+"/"+filename,f'tmp/{self.__username}/{self.__project_name}/images/{filename}')
             except:
                 logging.debug("couldn't download  %s",filename)
+
+            # download the image masks archive (.sgbdi file)
             try:
                 file_download(self.__images_dir_path+"/"+filename,f'tmp/{self.__username}/{self.__project_name}/image_masks/{filename_plus_sgbdi}')
             except:
                 logging.debug("couldn't download  %s",filename)
+
+            # download the segmented image corresponding to this image
             try:
                 file_download(self.__segmented_images_dir_path+"/"+filename_plus_png,f'tmp/{self.__username}/{self.__project_name}/segmented_images/{filename_plus_png}')
             except:
                 logging.debug("couldn't download  %s",filename_plus_png)
             
+        # Create ZIP archive
         with zipfile.ZipFile("tmp/downloads/"+zipfilename,'w') as zipf:
+            # put original images in the images/ subdirectory
             for foldername, subfolders,filenames in os.walk(f"tmp/{self.__username}/{self.__project_name}/images"):
                 for filename in filenames:
                     filePath = os.path.join(foldername,filename)
                     archive_path = os.path.join(self.__project_name,'images',filename)
                     zipf.write(filePath,arcname=archive_path)
+            # put SegBuilder masks archives in the image_masks/ subdirectory
             for foldername, subfolders,filenames in os.walk(f"tmp/{self.__username}/{self.__project_name}/image_masks"):
                 for filename in filenames:
                     filePath = os.path.join(foldername,filename)
                     archive_path = os.path.join(self.__project_name,'sgbdi',filename)
                     zipf.write(filePath,arcname=archive_path)
+            # put segmented images in the segmented_images/ subdirectory
             for foldername, subfolders,filenames in os.walk(f"tmp/{self.__username}/{self.__project_name}/segmented_images"):
                 for filename in filenames:
                     filePath = os.path.join(foldername,filename)
